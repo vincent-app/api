8edf27387f6f6763b86b7e103a43b032858b6b3e
Kris <info@getcoveredandsave.com>
2023-07-10 22:18:57 -0500

    Fixed?

---
+++ dist/app.js
@@ -0,0 +1,41 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = require("dotenv");
+/**
+ * Import routes
+ */
+const index_1 = __importDefault(require("./routes/index"));
+const app = (0, express_1.default)();
+/**
+ * Initialize dotenv
+ */
+(0, dotenv_1.config)();
+/**
+ * Express configuration (express.json, express.urlencoded, helmet, morgan, cors)
+ */
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, helmet_1.default)());
+app.use((0, morgan_1.default)("tiny"));
+app.use((0, cors_1.default)({
+    origin: "*", // Be sure to switch to your production domain
+}));
+/**
+ * Set express variables
+ * @param {string} host - Hostname
+ * @param {number} port - Port
+ */
+app.set("host", process.env.HOST || "localhost");
+app.set("port", process.env.PORT || 8080);
+/**
+ * Initialize routes
+ */
+app.use("/", index_1.default);
+exports.default = app;
---
+++ dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const consola_1 = require("consola");
+const client_1 = require("@prisma/client");
+const app_1 = __importDefault(require("./app"));
+/**
+ * Load Prsima Client and connect to Prisma Server if failed to connect, throw error.
+ */
+const prisma = new client_1.PrismaClient();
+prisma
+    .$connect()
+    .then(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$disconnect();
+    app_1.default.listen(app_1.default.get("port"), () => {
+        /**
+         *  Log infomation after everything is started.
+         */
+        if (process.env.NODE_ENV !== "test") {
+            consola_1.consola.log("----------------------------------------");
+            consola_1.consola.info(`Environment: ${app.get("env")}`);
+            consola_1.consola.info(`App URL: http://${app.get("host")}:${app.get("port")}`);
+            consola_1.consola.info(`Prisma: Connected`);
+            consola_1.consola.log("----------------------------------------");
+        }
+    });
+}))
+    .catch((e) => __awaiter(void 0, void 0, void 0, function* () {
+    console.error(e);
+    process.exit(1);
+}));
