a99ddf1556ec7f5f385ba5762cb13d344d281ff0
Kris <info@getcoveredandsave.com>
2023-07-21 19:38:22 -0500

    Fixed?

--- routes/webhook.ts
+++ routes/webhook.ts
@@ -12,66 +12,89 @@ const router = express.Router();
  * @description Simple route to test the API
  */
 router.post("/", async (req: Request, res: Response) => {
-  // check for secret header
-  if (!req.headers["x-webhook-secret"]) {
-    return res.status(403).json({
-      message: "❌ No secret header provided",
+  try {
+    // check for secret header
+    if (!req.headers["x-webhook-secret"]) {
+      return res.status(403).json({
+        message: "❌ No secret header provided",
+      });
+    }
+    if (req.headers["x-webhook-secret"] !== process.env.WEBHOOK_SECRET) {
+      return res.status(403).json({
+        message: "❌ Invalid secret header provided",
+      });
+    }
+
+    /**
+     * Check if email already exists
+     */
+
+    const userExists = await prisma.user.findUnique({
+      where: {
+        email: req.body.email as string,
+      },
+    });
+
+    if (userExists) {
+      return res.status(409).json({
+        message: "❌ User already exists",
+      });
+    }
+
+    const firstName = req.body.name.split(" ")[0] as string;
+    const lastName = req.body.name.split(" ")[1] as string;
+    const email = req.body.email as string;
+    const phoneNum = req.body.phoneNum as string;
+    const address = req.body.address.split("||")[0] as string;
+    const city = req.body.address.split("||")[1].split(", ")[0] as string;
+    const state = req.body.address.split("||")[1].split(", ")[1] as string;
+    const zipCode = req.body.address.split("||")[2] as string;
+    const birthday = new Date(req.body.birthday.unix * 1000) as Date;
+    const householdSize = req.body.householdSize as string;
+    const childrenCount = req.body.childrenCount as string;
+    const householdIncome = req.body.householdIncome as string;
+    const weightLbs = req.body.weightLbs as string;
+    const heightFeet = req.body.heightFeet as string;
+    const heightInches = req.body.heightInches as string;
+    const password = generator.generate({
+      length: 32,
+      numbers: true,
+    });
+
+    const passwordSalt = await bcrypt.genSalt(12);
+    const passwodHash = await bcrypt.hash(password, passwordSalt);
+
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        phoneNum,
+        address,
+        city,
+        state,
+        zipCode,
+        birthday,
+        householdSize,
+        childrenCount,
+        householdIncome,
+        weightLbs,
+        heightFeet,
+        heightInches,
+        password: passwodHash,
+      },
+    });
+
+    res.status(200).json({
+      message: "✅ User created",
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error",
     });
   }
-  if (req.headers["x-webhook-secret"] !== process.env.WEBHOOK_SECRET) {
-    return res.status(403).json({
-      message: "❌ Invalid secret header provided",
-    });
-  }
-
-  const firstName = req.body.name.split(" ")[0] as string;
-  const lastName = req.body.name.split(" ")[1] as string;
-  const email = req.body.email as string;
-  const phoneNum = req.body.phoneNum as string;
-  const address = req.body.address.split("||")[0] as string;
-  const city = req.body.address.split("||")[1].split(", ")[0] as string;
-  const state = req.body.address.split("||")[1].split(", ")[1] as string;
-  const zipCode = req.body.address.split("||")[2] as string;
-  const birthday = new Date(req.body.birthday.unix * 1000) as Date;
-  const householdSize = req.body.householdSize as string;
-  const childrenCount = req.body.childrenCount as string;
-  const householdIncome = req.body.householdIncome as string;
-  const weightLbs = req.body.weightLbs as string;
-  const heightFeet = req.body.heightFeet as string;
-  const heightInches = req.body.heightInches as string;
-  const password = generator.generate({
-    length: 32,
-    numbers: true,
-  });
-
-  const passwordSalt = await bcrypt.genSalt(12);
-  const passwodHash = await bcrypt.hash(password, passwordSalt);
-
-  const user = await prisma.user.create({
-    data: {
-      firstName,
-      lastName,
-      email,
-      phoneNum,
-      address,
-      city,
-      state,
-      zipCode,
-      birthday,
-      householdSize,
-      childrenCount,
-      householdIncome,
-      weightLbs,
-      heightFeet,
-      heightInches,
-      password: passwodHash,
-    },
-  });
-
-  return res.status(200).json({
-    message: "✅ User created",
-    user,
-  });
 });
 
 export default router;
